d4899a55dcc3d5c8c664d4dce8897fa4b4379281
Owen Scott <owens3364@gmail.com>
2023-01-06 19:03:30 -0600

    General item and Aged Brie tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -21,4 +21,81 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(itemQuality - 1, app.items[0].quality);
     }
+
+    @Test
+    void qualityDegradesByTwo_forGeneralItems_withValidQualityValues_afterTheSellByDate() {
+        int itemQuality = faker.number().numberBetween(2, 50);
+        int sellIn = faker.number().numberBetween(Integer.MIN_VALUE, -1);
+        GildedRose app = new GildedRose(new Item[] {
+            new Item(faker.name().name(), sellIn, itemQuality)
+        });
+        app.updateQuality();
+        assertEquals(itemQuality - 2, app.items[0].quality);
+    }
+
+    @Test
+    void qualityDegradesByOne_forGeneralItems_withValidQualityValueOne_afterTheSellByDate() {
+        int itemQuality = 1;
+        int sellIn = faker.number().numberBetween(Integer.MIN_VALUE, -1);
+        GildedRose app = new GildedRose(new Item[] {
+            new Item(faker.name().name(), sellIn, itemQuality)
+        });
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void quality_forGeneralItems_neverDegradesBelowZero() {
+        int itemQuality = 0;
+        int sellIn = faker.number().numberBetween(Integer.MIN_VALUE, Integer.MAX_VALUE);
+        GildedRose app = new GildedRose(new Item[] {
+            new Item(faker.name().name(), sellIn, itemQuality)
+        });
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void quality_forAgedBrie_increasesByOneWithAge_whenSellInIsPositive() {
+        int itemQuality = faker.number().numberBetween(0, 49);
+        int sellIn = faker.number().numberBetween(1, Integer.MAX_VALUE);
+        GildedRose app = new GildedRose(new Item[] {
+            new Item("Aged Brie", sellIn, itemQuality)
+        });
+        app.updateQuality();
+        assertEquals(itemQuality + 1, app.items[0].quality);
+    }
+
+    @Test
+    void quality_forAgedBrie_increasesByTwoWithAge_whenSellInIsNotPositive() {
+        int itemQuality = faker.number().numberBetween(0, 48);
+        int sellIn = faker.number().numberBetween(Integer.MIN_VALUE, 0);
+        GildedRose app = new GildedRose(new Item[] {
+            new Item("Aged Brie", sellIn, itemQuality)
+        });
+        app.updateQuality();
+        assertEquals(itemQuality + 2, app.items[0].quality);
+    }
+
+    @Test
+    void quality_forAgedBrie_doesNotIncreasePastFifty_whenSellInIsPositive() {
+        int itemQuality = 50;
+        int sellIn = faker.number().numberBetween(1, Integer.MAX_VALUE);
+        GildedRose app = new GildedRose(new Item[] {
+            new Item("Aged Brie", sellIn, itemQuality)
+        });
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    void quality_forAgedBrie_doesNotIncreasePastFifty_whenSellInIsNotPositive() {
+        int itemQuality = faker.number().numberBetween(49, 50);
+        int sellIn = faker.number().numberBetween(Integer.MIN_VALUE, 0);
+        GildedRose app = new GildedRose(new Item[] {
+            new Item("Aged Brie", sellIn, itemQuality)
+        });
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
 }

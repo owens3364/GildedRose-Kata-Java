94c4a4182a1fd66911ffe879d99f40051784eb73
Owen Scott <owens3364@gmail.com>
2023-01-07 17:41:30 -0600

    Remove the ugly logic

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -31,55 +31,6 @@ class GildedRose {
                 updateQualityOfConjured(item);
                 return;
         }
-
-        if (!item.name.equals(ItemType.AGED_BRIE.toString())
-            && !item.name.equals(ItemType.BACKSTAGE_PASSES.toString())) {
-            if (item.quality > 0) {
-                if (!item.name.equals(ItemType.SULFURAS.toString())) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.name.equals(ItemType.BACKSTAGE_PASSES.toString())) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!item.name.equals(ItemType.SULFURAS.toString())) {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (!item.name.equals(ItemType.AGED_BRIE.toString())) {
-                if (!item.name.equals(ItemType.BACKSTAGE_PASSES.toString())) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals(ItemType.SULFURAS.toString())) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
     }
 
     private void updateQualityOfGeneral(Item item) {
